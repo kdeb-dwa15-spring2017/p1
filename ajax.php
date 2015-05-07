b40e85713b3e32f59ddacad18cccb0d131535fe1
Kate deBethune <kdebethune@gmail.com>
2015-05-07 06:14:21 -0400

    ajax tweaks

--- ajax.php
+++ ajax.php
@@ -1,1 +1,5 @@
-<?php header('Access-Control-Allow-Origin: *'); ?> {"book_notes": [{ "id": "CcM4H6eswCUC", "notes": "Lorum Ipsum Dolor" }, { "id": "CcM4H6eswCUC", "notes": "Nulla fermentum consequat aenean" }]}
+<?php header('Access-Control-Allow-Origin: *'); ?> {"book_notes": [{ "id": "CcM4H6eswCUC", "notes": "Lorum Ipsum Dolor" },  { "id": "9SGbSjx1CYcC", "notes": "egestas dui nisi primis nisl, interdum habitant tincidunt ultrices nostra proin" }]}
+<!DOCTYPE html>
+<html>
+<body>
+
